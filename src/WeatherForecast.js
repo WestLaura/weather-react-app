820d9651a13d768320a79f77339cacb4917ac9e2
WestLaura <132126791+WestLaura@users.noreply.github.com>
2023-10-06 16:38:10 +0100

    fixed weather icons issue

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "8c78e9e7e9928cd1a2a6f923072c3dec ";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
